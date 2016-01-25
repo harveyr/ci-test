5432e1333a166553854eb24bcbaaeb426888066b
Harvey Rogers <harveyr@gmail.com>
2016-01-25 14:39:20 -0800

    ==

--- tests.py
+++ tests.py
@@ -1,6 +1,10 @@
-# copypasta from the docs: https://docs.python.org/2/library/unittest.html
 import unittest
 
+# prove that requirements were installed
+import click
+
+
+# copypasta from the docs: https://docs.python.org/2/library/unittest.html
 class TestStringMethods(unittest.TestCase):
 
   def test_upper(self):
